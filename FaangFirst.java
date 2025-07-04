14597508682f3debea926a3dc17f4b36e0dae64e
Divakar <bdivakar0786@gmail.com>
2025-07-04 12:18:13 +0530

    First FAANG Preparation file

---
+++ FaangFirst.java
@@ -0,0 +1,8 @@
+public class FaangFirst {
+    public static void main(String[] args) {
+        System.out.println("Namma goal: FAANG developer!\n" +
+                "Oru kalam varum — \"Divakar, SDE @ FAANG\" — mark my words da! \uD83D\uDE0E\uD83D\uDD25");
+        System.out.println("“A day will come — ‘Divakar, SDE at FAANG’ — remember my words, bro!” \uD83D\uDE0E\uD83D\uDD25");
+    }
+
+}
